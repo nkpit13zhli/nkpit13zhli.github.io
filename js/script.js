ca7e14a3613578330f7b5bde9a64e1e7458b5164
Zebh Lindqvist <zebh.lindqvist@outlook.com>
2015-11-03 18:58:23 +0100

    Bättre styrning av mobilmeny
    Menyn stängs vid klick utanför menyn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).on('click touchstart', '#menu-button', function(){
-	$('#menu-items').fadeToggle(150);
+	$('#menu-items').toggle(150);
+});
+
+$(document).on('click touchstart', function(e){
+	var o = $(e.target);
+	if( !$(o).is('#menu-button') && !$(o).is('#menu-items') )
+	{
+		$('#menu-items').hide(150);
+	}
 });
