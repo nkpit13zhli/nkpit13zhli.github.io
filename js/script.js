a3bd613270c0e85ca81338ff54ab8f7761c5b4ba
Zebh Lindqvist <zebh.lindqvist@outlook.com>
2015-11-03 21:23:49 +0100

    Stäng mobilmenyn vid klick utanför

--- js/script.js
+++ js/script.js
@@ -6,14 +6,13 @@ $(document).on('click touchstart', '#menu-button', function(e){
 var flag = false;
 
 $(document).on('click touchstart', function(e){
-
 	if(!flag)
 	{
 		flag = true;
 		setTimeout(function(){ flag = false; }, 100);
 
 		var o = $(e.target);
-		if( $('#menu-button').css('display') != 'none' && !$(o).is('#menu-button') && !$(o).is('#menu-items') && !$('#menu-items').has(o) )
+		if($('#menu-button').css('display') != 'none' && !$(o).is('#menu-button') && !$(o).is('#menu-items') && !$.contains('#menu-items', o) )
 		{
 			$('#menu-items').hide(150);
 		}
