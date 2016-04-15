fc89379e1b69fbd8b400a9bf6212e30de3a779c0
Zebh Lindqvist <zebh.lindqvist@outlook.com>
2016-04-15 10:49:56 +0200

    Added slider
    Added a nice slider with reviews at the bottom of the page :3

--- js/script.js
+++ js/script.js
@@ -22,3 +22,76 @@ $(document).on('click touchstart', function(e){
 		}
 	}
 });
+
+  /*
+  START Slider
+  */
+  var lastSlide = 0;
+  var currentSlide = 0;
+  var slides = 0;
+  var duration = 800;
+
+  function scrollSlide(action)
+  {
+    if(action === 'left' && currentSlide > 0)
+    {
+      currentSlide--;
+    }
+    else if(action === 'right'  && currentSlide < slides - 1)
+    {
+      currentSlide++;
+    }
+    
+    if(currentSlide === 0)
+    {
+        $('#arrowLeft').hide();
+    }
+    else
+    {
+        $('#arrowLeft').show();
+    }
+    if(currentSlide === slides - 1)
+    {
+        $('#arrowRight').hide();
+    }
+    else
+    {
+        $('#arrowRight').show();
+    }
+    
+    if(currentSlide !== lastSlide || action === 'update')
+    {
+      var dur = action === 'update' ? 0 : duration;
+      
+      var w = $('.slides-wrapper');
+      var width = w.outerWidth() + 5;
+      var totWidth = width * currentSlide;
+      w.animate({scrollLeft: totWidth}, dur);
+      lastSlide = currentSlide;
+    }
+  }
+
+  $(document).on("click touchstart","#arrowLeft",function(e) {
+    e.preventDefault();
+    scrollSlide('left');
+  });
+
+  $(document).on("click touchstart","#arrowRight",function(e) {
+    e.preventDefault();
+    scrollSlide('right');
+  });
+
+  $( document ).ready(function() {
+    slides = $('#slider > .slides-wrapper').children('.slide').length;
+    if(slides > 0)
+    {
+      scrollSlide('update');
+    }
+  });
+
+  $( window ).resize(function() {
+    scrollSlide('update');
+  });
+  /*
+  END Slider
+  */
